3bffa825edc548ea6648b4bdaf0ebea9353367a9
juice-world <87857753+juice-world@users.noreply.github.com>
2021-07-27 13:45:34 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,64 +1,115 @@
-var dino;
-var ground;
-var groundImage;
-var dinoRunning;
-var invisGround;
-var cloud;
-var cloudImage;
+var bow , arrow,  scene;
+var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
 
-function setup(){
-createCanvas(750,300);
-dino=createSprite(50,260,20,20);
-dino.scale=0.5;
-
-dino.addAnimation("running",dinoRunning);
-
-ground=createSprite(200,280,600,20);
-ground.addImage("ground",groundImage);
-ground.x=ground.width/2;
-ground.velocityX=-2;
-
-invisGround=createSprite(200,290,600,11);
-invisGround.visible=false;
-}
-
-function draw(){
-  background("#d2aa6d");
-  if(keyDown("space") && dino.y>=201){
-  dino.velocityY=-6;
-
-  }
-  dino.velocityY=dino.velocityY+0.4;
-  dino.collide(invisGround);
-
-  if(ground.x<0){
-    ground.x=ground.width/2;
-  }
-  spawnCloud();
-  drawSprites();
-  
-  
-
-}
+var score=0;
+var arrow;
 function preload(){
-  groundImage=loadImage("ground2.png");
-dinoRunning=loadAnimation(
-
-  "trex1.png","trex2.png","trex3.png"
-);
-cloudImage=loadImage("cloud.png");
+  
+  backgroundImage = loadImage("background0.png");
+  arrowImage = loadImage("arrow0.png");
+  bowImage = loadImage("bow0.png");
+  red_balloonImage = loadImage("red_balloon0.png");
+  green_balloonImage = loadImage("green_balloon0.png");
+  pink_balloonImage = loadImage("pink_balloon0.png");
+  blue_balloonImage = loadImage("blue_balloon0.png");
+  
 }
 
 
-function spawnCloud(){
 
-if(frameCount%60===0){
-  cloud=createSprite(750,101,55,24);
-  cloud.addImage(cloudImage);
-  cloud.velocityX=-2;
-  cloud.y=Math.round(random(5,60));
-  cloud.scale=0.7;
-  cloud.depth=dino.depth;
-  dino.depth=dino.depth+1    
+function setup() {
+  createCanvas(400, 400);
+  
+  //creating background
+  scene = createSprite(0,0,400,400);
+  scene.addImage(backgroundImage);
+  scene.scale = 2.5
+  
+  // creating bow to shoot arrow
+  bow = createSprite(380,220,20,50);
+  bow.addImage(bowImage); 
+  bow.scale = 1;
+  
+   score = 0    
 }
+
+function draw() {
+ background(0);
+  // moving ground
+    scene.velocityX = -3 
+
+    if (scene.x < 0){
+      scene.x = scene.width/2;
+    }
+  
+  //moving bow
+  bow.y = World.mouseY
+  
+   // release arrow when space key is pressed
+  if (keyDown("space")) {
+    createArrow();
+    
+  }
+   
+  //creating continous enemies
+  var select_balloon = Math.round(random(1,4));
+  
+  if (World.frameCount % 100 == 0) {
+    if (select_balloon == 1) {
+      redBalloon();
+    } else if (select_balloon == 2) {
+      greenBalloon();
+    } else if (select_balloon == 3) {
+      blueBalloon();
+    } else {
+      pinkBalloon();
+    }
+  }  
+    
+  drawSprites();
+  text("Score: "+ score, 300,50);
+}
+
+
+ function createArrow() {
+  var arrow= createSprite(100, 100, 60, 10);
+  arrow.addImage("arrow0.png");
+  arrow.x = 360;
+  arrow.y=bow.y;
+  arrow.velocityX = -4;
+  arrow.lifetime = 100;
+  arrow.scale = 0.5;
+  arrow.mouseY=1
+}
+
+function redBalloon() {
+  var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  red.addImage(red_balloonImage);
+  red.velocityX = 3;
+  red.lifetime = 150;
+  red.scale = 0.1;
+}
+
+function blueBalloon() {
+  var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  blue.addImage(blue_balloonImage);
+  blue.velocityX = 3;
+  blue.lifetime = 150;
+  blue.scale = 0.1;
+}
+
+function greenBalloon() {
+  var green = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  green.addImage(green_balloonImage);
+  green.velocityX = 3;
+  green.lifetime = 150;
+  green.scale = 0.1;
+}
+
+function pinkBalloon() {
+  var pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  pink.addImage(pink_balloonImage);
+  pink.velocityX = 3;
+  pink.lifetime = 150;
+  pink.scale = 1
 }
